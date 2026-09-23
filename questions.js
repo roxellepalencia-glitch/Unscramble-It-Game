fe0d3c24edd1bee9abf8d5cdf8a27cae9550ab54
roxellepalencia-glitch <roxellepalencia@gmail.com>
2026-09-23 12:18:52 -0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -12,7 +12,7 @@
 // The host will NOT see this list during the game.
 
 const WORDS = [
-  ['DVILETAA', 'To confirm or verify that something is true or correct'],
+  ['VALIDATE', 'To confirm or verify that something is true or correct'],
   ['TEAMWORK', 'People cooperating toward a shared goal'],
   ['COMMUNICATION', 'Sharing information clearly'],
   ['ESCALATION', 'Moving an issue to a higher level for help'],
